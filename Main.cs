fe07cca8d35362157ceaa21d63831eb94b6c3fdb
pr0kuk <alex-shch@yandex.ru>
2022-11-05 17:01:55 +0300

    added main.cs draft

---
+++ Main.cs
@@ -0,0 +1,17 @@
+namespace Driver
+{
+    using System;
+    using CSV;
+    using Microsoft.Quantum.Simulation.Simulators.QCTraceSimulators;
+
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        private static void SingleResourceTest()
+        {
+        }
+    }
+}
