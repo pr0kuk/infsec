a06e81658a847853019a143cbb4d103ffbfd4b76
pr0kuk <alex-shch@yandex.ru>
2022-11-05 17:06:27 +0300

    added singleresourcetest()

--- Main.cs
+++ Main.cs
@@ -12,6 +12,10 @@ namespace Driver
 
         private static void SingleResourceTest()
         {
+            QCTraceSimulator estimator = GetTraceSimulator(full_depth);
+            var res = runner(estimator, n, isControlled).Result;
+            string thisCircuitCosts = estimator.ToString;
+            Console.WriteLine(thisCircuitCosts);
         }
     }
 }
